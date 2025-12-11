ff9653183472666d7eb81fefedb2173045570939
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2025-12-11 08:41:14 +0100

    updated

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL'), 'http://localhost:5173',],
+    'allowed_origins' => [env('FRONTEND_URL'), 'https://every-city-frontend.vercel.app', 'https://www.every-city-frontend.vercel.app', 'http://localhost:5173',],
 
     'allowed_origins_patterns' => [],
 
